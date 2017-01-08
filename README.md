bcc5c5be3c5f9428685cacbf0027a177a958fa41
Béla Varga <netzzwerg@googlemail.com>
2017-01-08 21:30:20 +0100

    Added assets, browser sync and changed npm scripts.

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # munichjs.org
+
 The MunichJS Website.
 
+## Set up
+
+Clone the repo, install the npm modules.
+
+```bash
 npm install
-node build.js
+```
+
+## Develop
+
+To build the static site and start the preview server:
+
+```bash
+npm start
+```
+
+## Deploy
+
+To build the static site:
+
+```bash
+npm run build
+```
+
+## Clean
+
+To remove the generated content:
+
+```bash
+npm run clean
+```
--- build.js
+++ build.js
@@ -1,26 +1,52 @@
-var metalsmith = require('metalsmith');
-var layouts = require('metalsmith-layouts');
-var markdown = require('metalsmith-markdown');
-var metadata = require('metalsmith-metadata');
+#!/usr/bin/env node
 
-var siteBuild = metalsmith(__dirname)
-  .metadata({
-    title: 'MunichJS'
-  })
+'use strict';
+
+var devBuild = ((process.env.NODE_ENV || '').trim().toLowerCase()) !== 'production';
+var pkg = require('./package.json');
+
+console.log((devBuild ? 'Development' : 'Production'), 'build, version', pkg.version);
+
+var metalsmith  = require('metalsmith');
+var layouts     = require('metalsmith-layouts');
+var markdown    = require('metalsmith-markdown');
+var metadata    = require('metalsmith-metadata');
+var assets      = require('metalsmith-assets');
+var browsersync = devBuild ? require('metalsmith-browser-sync') : null;
+
+var pageMeta = {
+  title   : 'MunichJS',
+  version : pkg.version,
+  domain  : devBuild ? 'http://127.0.0.1' : 'https://munichjs.org',
+};
+
+var ms = metalsmith(__dirname)
+  .clean(true)
+  .metadata(pageMeta)
   .source('./contents')
   .destination('./build')
   .use(markdown())
   .use(metadata({
-    events: 'events.json'
+    events  : 'events.json'
   }))
   .use(layouts({
-    engine: 'pug'
+    engine  : 'pug'
   }))
-  .build(function (err) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      console.log('Site build complete!');
-    }
-  });
+  .use(assets({
+    source  : './assets'
+  }));
+
+if(devBuild) ms
+  .use(browsersync({
+    server  : './build',
+    files   : ['./contents/**/*.md', './layouts/**/*.pug']
+  }))
+
+ms.build(function (err) {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log('Site build complete!');
+  }
+});
